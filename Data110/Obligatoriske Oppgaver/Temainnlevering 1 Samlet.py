574ff499dd7ce6a96ac40023d5a33c6293c587ba
Emmanator <andreassen1001@gmail.com>
2022-01-28 00:28:35 +0100

    Opptadert koomentarer på oppgave 3

--- Data110/Obligatoriske Oppgaver/Temainnlevering 1 Samlet.py
+++ Data110/Obligatoriske Oppgaver/Temainnlevering 1 Samlet.py
@@ -30,6 +30,7 @@ print("""Jakob
 Andreassen""")
 
 # Oppgave 1.4
+# Enkel metode for å printe flere strings samtidig.
 print("Jakob", "Andreassen", sep='\n')
 
 # Oppgave 2
@@ -50,12 +51,12 @@ euro_kurs = 0.098
 dollar_kurs = 0.11
 kroner = 250
 
-# # Definerer konverterte variabler
+# Definerer konverterte variabler
 euro_kon = kroner * euro_kurs
 dollar_kon = kroner * dollar_kurs
 
 # Print hva X kroner er i euro og dollar etter kurs variabel
-# Hadde lyst til å få med 2 desimaler siden det er det som står i oppgaveteksten
+# Bruker print(f'{tekst:.2f}') for å få med 2 desimaler siden det er det som står i oppgaveteksten
 print(f'{kroner} kroner tilsvarer {euro_kon:.2f} Euro og {dollar_kon:.2f} Dollar.')
 print(f'{kroner} NOK tilsvarer {euro_kon:.2f}\N{euro sign} og {dollar_kon:.2f}\N{dollar sign}.')
 
