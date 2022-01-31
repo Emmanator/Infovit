7ecaf07c2ad3aa5b5d742fe327dbcf2208832c24
Emmanator <andreassen1001@gmail.com>
2022-01-31 12:27:15 +0100

    lab oppgave 2

--- Data110/Laboppgaver/Lab 2 Oppgaver.py
+++ Data110/Laboppgaver/Lab 2 Oppgaver.py
@@ -5,3 +5,12 @@
 # Oppgave 3
 import random
 
+tegn = '?' * (random.randint(10,60))
+print(tegn)
+lengde = len(tegn)
+svar = int(input("Guess the number of ?s: "))
+
+if svar == lengde:
+    print("That's correct!")
+else:
+    print("That's false!")
