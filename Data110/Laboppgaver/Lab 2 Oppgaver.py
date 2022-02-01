8d60869b595944dd2d21ca60083292903fd6dd83
Emmanator <andreassen1001@gmail.com>
2022-02-01 13:15:27 +0100

    lab 2

--- Data110/Laboppgaver/Lab 2 Oppgaver.py
+++ Data110/Laboppgaver/Lab 2 Oppgaver.py
@@ -28,13 +28,14 @@
 
 # Oppgave 5
 import sys
-# tst = int(input('Skriv et tresifret tall: '))
+# int(input)) aksepterer bare integers så trenger ikke å skape error melding enda
 tst = int(input('Skriv et tresifret tall:'))
 
-if len(str(tst)) != 3:
+if len(str(tst)) != 3:  # Skjekker om tst er 3 karakterer lang
     print("Error! må være 3 karakterer")
     sys.exit()
 
+# matte magi
 s1 = ((tst % 100) % 10)
 s2 = ((tst // 10) % 10)
 s3 = (tst // 100)
@@ -45,4 +46,3 @@ print(f'Permutasjoner: '
       f'{s2}{s1}{s3},'
       f'{s1}{s3}{s2},'
       f'{s1}{s2}{s3}.')
-ttest
