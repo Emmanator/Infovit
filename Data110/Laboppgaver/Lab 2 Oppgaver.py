7c4a66c0dd5490601f6175634dc85142273c0014
Emmanator <andreassen1001@gmail.com>
2022-02-01 12:17:23 +0100

    lab oppgave 2

--- Data110/Laboppgaver/Lab 2 Oppgaver.py
+++ Data110/Laboppgaver/Lab 2 Oppgaver.py
@@ -3,14 +3,31 @@
 # m = (int(input('Til:')))
 
 # Oppgave 3
-import random
+# import random
+#
+# tegn = '?' * (random.randint(10,60))
+# print(tegn)
+# lengde = len(tegn)
+# svar = int(input("Guess the amount of ?: "))
+#
+# if svar == lengde:
+#     print("That's correct!")
+# else:
+#     print("That's false!")
+# print(f'{lengde} is the correct answer.')
 
-tegn = '?' * (random.randint(10,60))
-print(tegn)
-lengde = len(tegn)
-svar = int(input("Guess the number of ?s: "))
+# Oppgave 4
+# import math
+#
+# n = int(input('Tall 1: '))
+# m = int(input('Tall 2: '))
+# nm = int(str(n) + str(m))
+# mn = int(str(m) + str(n))
+# print(f'{nm} og {mn}')
+# print(f'Kvadraroten av {nm} * {mn} er {math.sqrt(mn * nm):.2f}')
 
-if svar == lengde:
-    print("That's correct!")
-else:
-    print("That's false!")
+# Oppgave 5
+tre_sifret_tall = int(input('skriv et tresifret tall: '))
+s1 = ((tre_sifret_tall % 100) % 10)
+
+print(s1)
