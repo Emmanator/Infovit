c2372002b44d722dc5dff5e42a27a563bc14094b
Emmanator <andreassen1001@gmail.com>
2022-02-01 13:35:30 +0100

    lab 2

--- Data110/Laboppgaver/Lab 2 Oppgaver.py
+++ Data110/Laboppgaver/Lab 2 Oppgaver.py
@@ -27,22 +27,42 @@
 # print(f'Kvadraroten av {nm} * {mn} er {math.sqrt(mn * nm):.2f}')
 
 # Oppgave 5
-import sys
-# int(input)) aksepterer bare integers så trenger ikke å skape error melding enda
-tst = int(input('Skriv et tresifret tall:'))
+# import sys
+# # int(input)) aksepterer bare integers så trenger ikke å skape error melding enda
+# tst = int(input('Skriv et tresifret tall:'))
+#
+# if len(str(tst)) != 3:  # Skjekker om tst er 3 karakterer lang
+#     print("Error! må være 3 karakterer")
+#     sys.exit()
+#
+# # matte magi
+# s1 = ((tst % 100) % 10)
+# s2 = ((tst // 10) % 10)
+# s3 = (tst // 100)
+# print(f'Permutasjoner: '
+#       f'{s3}{s2}{s1},'
+#       f'{s3}{s1}{s2},'
+#       f'{s2}{s3}{s1},'
+#       f'{s2}{s1}{s3},'
+#       f'{s1}{s3}{s2},'
+#       f'{s1}{s2}{s3}.')
 
-if len(str(tst)) != 3:  # Skjekker om tst er 3 karakterer lang
-    print("Error! må være 3 karakterer")
-    sys.exit()
+# Oppgave 6
+import random
+paper = 1
+scissor = 2
+rock = 3
+machine = random.randint(1, 3)
+# Takes user input
+user = int(input('Choose 1=paper, 2=scissor, 3=rock: '))
+print(f'Machine chose {machine}')
 
-# matte magi
-s1 = ((tst % 100) % 10)
-s2 = ((tst // 10) % 10)
-s3 = (tst // 100)
-print(f'Permutasjoner: '
-      f'{s3}{s2}{s1},'
-      f'{s3}{s1}{s2},'
-      f'{s2}{s3}{s1},'
-      f'{s2}{s1}{s3},'
-      f'{s1}{s3}{s2},'
-      f'{s1}{s2}{s3}.')
+# Checks if the final value come out true or false
+user_win = (user == (machine % 3) + 1)
+tied = user == machine
+machine_win = (machine == (user % 3) +1)
+
+# Prints the results with either true or false
+print(user_win)
+print(tied)
+print(machine_win)
