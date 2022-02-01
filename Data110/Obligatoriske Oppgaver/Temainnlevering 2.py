3f731d70035ef0df0ad35b4970ac0ab59c239b56
Emmanator <andreassen1001@gmail.com>
2022-02-01 14:18:18 +0100

    Temainnlevering 2

--- Data110/Obligatoriske Oppgaver/Temainnlevering 2.py
+++ Data110/Obligatoriske Oppgaver/Temainnlevering 2.py
@@ -11,7 +11,7 @@ print(f'Arealet til en sirkel med radius {r} er {a:.3f}.')
 # Benytter len() for å få en variabel som leser hvor mange bokstaver det er i sentence
 sentence = str(input('Skriv en setning: '))
 input_lengde = len(sentence)
-guess = int(input('Gjett antall bokstaver '))
+guess = int(input('Gjett antall bokstaver: '))
 
 # Benytter if else statement til å se om du gjetter riktig.
 if guess == input_lengde:
@@ -22,9 +22,9 @@ else:
 # Oppgave 3
 import random  # Liker å ha imports på toppen av scriptet, men har den her for oppgavens skyld.
 tall = int(input('Gi meg et tall: '))  # tar bare integer inputs
-rand = random.randint(1, 100)  # random tall fra 1 til 100
+rand = random.randint(1, 9)  # random tall fra 1 til 100
 
 # Setter verdien produsert av random og legger den bak input tallet
 kombinert = int(str(tall) + str(rand))
 # deler kombinert verdi på det originale tallet
-print(kombinert / tall)
+print(f'{kombinert}/{tall} = {kombinert / tall}')
