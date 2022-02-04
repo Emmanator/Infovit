e77f92f5fdafa8b3b05319b6419ef97fd463acc5
Emmanator <andreassen1001@gmail.com>
2022-02-04 17:02:12 +0100

    Tema 3

--- Data110/Obligatoriske Oppgaver/Temainnlevering 3 Oppg 2.py
+++ Data110/Obligatoriske Oppgaver/Temainnlevering 3 Oppg 2.py
@@ -4,16 +4,16 @@ bodd = int(input('Hvor mange år har du bodd i Tulleby?: '))
 
 bystyre_akrav = 25
 bystyre_bkrav = 5
-ordfører_akrav = 30
-ordfører_bkrav = 9
+ordfrer_akrav = 30
+ordfrer_bkrav = 9
 
 # Alderskrav
 # Bruker krav variablene definert tiligere fpr å gjøre enkle matte utregninger
 bystyre = bystyre_akrav - alder
-ordfører = ordfører_akrav - alder
+ordfrer = ordfrer_akrav - alder
 # Bodkrav
 bybkrav = bystyre_bkrav - bodd
-ordbkrav = ordfører_bkrav - bodd
+ordbkrav = ordfrer_bkrav - bodd
 
 # if, elif, else statements som skjekker hva din alder er kommer tilbake med
 # en respons for hvor mange år til man blir kvalifisert.
@@ -21,7 +21,7 @@ if alder >= 30 and bodd >= 9:
     print('Du kan bli ordfører eller sitte i bystyret.')
 elif alder >= 25 and bodd >= 5:
     print('Du kan sitte i bystyret.')
-    print(f'Prøv igjen om {max(ordfører, ordbkrav)} år for å bli ordfører.')
+    print(f'Prøv igjen om {max(ordfrer, ordbkrav)} år for å bli ordfører.')
 else:
     print('Du er hverken kvalifisert til å sitte i bystyret eller ordfører.')
-    print(f'Kvalifisert til bystyret om {max(bystyre,bybkrav)} år eller ordfører om {max(ordfører, ordbkrav)}.')
+    print(f'Kvalifisert til bystyret om {max(bystyre,bybkrav)} år eller ordfører om {max(ordfrer, ordbkrav)}.')
