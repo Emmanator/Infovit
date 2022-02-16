35c649c7d6e4d92fa40265094560a0809711cdab
Emmanator <andreassen1001@gmail.com>
2022-02-16 17:18:26 +0100

    lab 4

--- Data110/Laboppgaver/Lab 4 Oppgaver.py
+++ Data110/Laboppgaver/Lab 4 Oppgaver.py
@@ -7,23 +7,45 @@
 #         print("you can't drink")
 
 # b)
+import random
+
+
 def myndig(alder):
     return alder >= 18
+
 
 if myndig(17):
     print('you can drink')
 else:
     print("you can't drink")
 
+
 # Oppgave 2
 def siffer(tall, n):
     return int(str(tall)[n - 1])
 
+
 # Oppgave 3
-def fullname(firstname, lastname, middlename = None):
+def fullname(firstname, lastname, middlename=None):
     if middlename is None:
         print(firstname, lastname)
     else:
         print(firstname, middlename, lastname)
 
+
 # Oppgave 4
+Spar = '\u2660'
+Ruter = '\u2666'
+Kløver = '\u2663'
+Hjerter = '\u2665'
+farger = [Spar, Ruter, Kløver, Hjerter]
+tall = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+
+def tilfeldigKort(kort=None):
+    if kort is None:
+        kort = random.choice(farger)
+    return kort + random.choice(tall)
+
+
+print(tilfeldigKort())
