ca156709a1d9d2f633db31548e8954a7e723b8bd
Emmanator <andreassen1001@gmail.com>
2022-03-11 11:40:48 +0100

    ti 5

--- Data110/Obligatoriske Oppgaver/jij014_ti5.py
+++ Data110/Obligatoriske Oppgaver/jij014_ti5.py
@@ -6,4 +6,30 @@ def antall_vokaler(tekst: str):
             teller += 1
     return teller
 
+
 # Oppgave 2
+TV = \
+"""
+Tulleveien Velforening
+leder: Kari
+kasserer: Ole
+IT-ansvarlig: Liv
+parkeringsansvarlig: Kari
+arrangementsansvarlig: Liv
+hagekonsulent: Kari
+brannansvarlig: Kari
+"""
+# Hadde det ikke vært mye bedre og lagret vervene som en variabel og navn som en variabel
+# 
+
+def verv(navn: str):
+    split = list(TV.splitlines())
+    roller = []
+    for i in split:
+        if i.endswith(navn):
+            jobb = i.split(':')
+            roller.append(jobb[0])
+    return roller
+
+
+print(verv('Liv'))
