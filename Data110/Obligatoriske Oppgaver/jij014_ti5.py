309ec47b9fce0d46a4b8b3b30ed1c66ea4f4cdab
Emmanator <andreassen1001@gmail.com>
2022-03-11 11:46:57 +0100

    ti 5

--- Data110/Obligatoriske Oppgaver/jij014_ti5.py
+++ Data110/Obligatoriske Oppgaver/jij014_ti5.py
@@ -2,7 +2,7 @@
 def antall_vokaler(tekst: str):
     teller = 0
     for i in tekst:
-        if i in "aeiouyæøåAEIOUYÆØÅ":
+        if i in "aeiouyæøåAEIOUYÆØÅ":  # skjekker bare om i er i vokal lista og legger til +1 til teller
             teller += 1
     return teller
 
@@ -20,15 +20,14 @@ hagekonsulent: Kari
 brannansvarlig: Kari
 """
 # Hadde det ikke vært mye bedre og lagret vervene som en variabel og navn som en variabel
-# 
 
 def verv(navn: str):
-    split = list(TV.splitlines())
-    roller = []
-    for i in split:
-        if i.endswith(navn):
-            jobb = i.split(':')
-            roller.append(jobb[0])
+    split = list(TV.splitlines())   # splitter for hver line i mutli-line stringen ovenfor
+    roller = []                     # list variabel for å appende til
+    for i in split:                 # siden split gjør det om til en liste er "i" hver linje i TV
+        if i.endswith(navn):        # skjekker for linjer som slutter med navn
+            jobb = i.split(':')     # splitter på kolon tegnet
+            roller.append(jobb[0])  # legger til det venstre elementet fra forrige splitt
     return roller
 
 
