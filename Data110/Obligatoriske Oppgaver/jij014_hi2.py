331a21ceffed79ad33cf286c4c036ed5cb55a057
Emmanator <andreassen1001@gmail.com>
2022-03-28 21:20:59 +0200

    hi2

--- Data110/Obligatoriske Oppgaver/jij014_hi2.py
+++ Data110/Obligatoriske Oppgaver/jij014_hi2.py
@@ -1,40 +1,37 @@
-import sys
-
-emner = ['INFO100', 'INFO104', 'ECON100', 'JAP100', 'JAP110', 'INFO120', 'INFO125', 'KOGVIT101', 'JAP120', 'ECON200',
-         'ECON220', 'ECON240', 'ECON300', 'ECON320', 'ECON340']
-karakterer = {'INFO100': 'C', 'INFO104': 'B', 'ECON100': 'B', 'JAP100': 'C', 'JAP110': 'C', 'KOGVIT101': 'B',
-              'ECON300': 'F', 'ECON220': 'D'}
-fagområde = {'JAP': 'japansk', 'INF': 'informasjonsvitenskap', 'ECO': 'økonomi', 'KOG': 'kognitivvitenskap'}
+emner = []
+karakterer = {}
+fagområde = {}
 
 
-def emne():
-    return
+def get_subjects(area: str | None, level: str | None) -> list[str]:
+    matching = []
+
+    for i in emner:
+        area_code = i[:-3]
+        if (area is None or fagområde[area_code] == area) and (level is None or i[-3] == level[0]):
+            matching.append(i)
+
+    return matching
 
 
-def emneliste():
-    global emner
-    global karakterer
-    global fagområde
-    inv_map = {v: k for k, v in fagområde.items()}  # konverterer fagområde liste til det omvendte
-    sorter = []
-    print('Velg fag og/eller emnenivå (<enter> for alle)')
-    field = str(input('Fag: ') or None)
-    if field is not None:
-        field = inv_map.get(field)
-    nivå = input('Nivå: ') or None
+def get_graded_subjects(to_grade: list[str]) -> dict[str, str | None]:
+    graded = {}
 
-    for p in emner:
-        if (field is None or p.startswith(field)) and (nivå is None or p[-3] == nivå[0]):
-            if p in karakterer:
-                # print(f'{p} {karakterer[p]}')
-                sorter.append(f'{p} {karakterer[p]}')
-            else:
-                # print(p)
-                sorter.append(f'{p}')
+    for i in to_grade:
+        if i in karakterer:
+            graded[i] = karakterer[i]
+        else:
+            graded[i] = None
 
-    for s in sorted(sorter):
-        print(s)
-    return
+    return graded
+
+
+def show_graded(graded: dict[str, str | None]):
+    for sub in sorted(graded.keys()):
+        if graded[sub] is None:
+            print(sub)
+        else:
+            print(f'{sub} {graded[sub]}')
 
 
 def add():
@@ -44,33 +41,35 @@ def add():
     return
 
 
-def karakter():
-    emne = input('Emne: ')
-    while emne not in emner:
-        emne = input('Må være et gyldig emne: ')
+def karakter_sett():
+    emne2 = input('Emne: ')
+    while emne2 not in emner:
+        emne2 = input('Må være et gyldig emne: ')
 
     sett_karakter = input('Karakter (<enter> for å slette): ')
     while sett_karakter not in ['A', 'B', 'C', 'D', 'E', 'F', '']:
         sett_karakter = input('Må være karakter A-F: ')
 
     if sett_karakter == '':
-        if emne in karakterer:
-            del karakterer[emne]
+        if emne2 in karakterer:
+            del karakterer[emne2]
         return
 
-    karakterer[emne] = sett_karakter
+    karakterer[emne2] = sett_karakter
 
 
-def karaktersnitt():
+def karaktersnitt(graded):
     global karakterer
-    verdi = karakterer.values()
     nummer = 0
     abc_grade = {'A': 96, 'B': 86, 'C': 76, 'D': 66, 'E': 56, 'F': 50}
-
-    for i in verdi:
+    le = 0
+    for i in graded.values():
+        if i is None:
+            continue
         a = abc_grade[i]
         nummer += a
-    kalk = nummer / len(verdi)
+        le += 1
+    kalk = nummer / le
     kalk = int(kalk)
 
     match kalk:
@@ -88,29 +87,108 @@ def karaktersnitt():
             return 'F'
 
 
-def velg():
+def field_get():
+    field = input('Fag: ')
+    while field != '' and field not in fagområde.values():
+        field = input('Fag (må være gyldig emne): ')
+    field = field or None
+    return field
+
+
+def nivå_get():
+    nivå = input('Nivå: ')
+    while nivå != '' and nivå[0] not in ['1', '2', '3']:
+        nivå = input('Nivå (må være gyldig nivå: ')
+    nivå = nivå or None
+    return nivå
+
+
+def lagre():
+    with open('hi2_emner.txt', 'w', encoding='utf-8') as a:
+        for i in emner:
+            a.write(i)
+            a.write('\n')
+    with open('hi2_fagområde.txt', 'w', encoding='utf-8') as s:
+        for k, v in fagområde.items():
+            s.write(f'{k} {v}')
+            s.write('\n')
+    with open('hi2_karakterer.txt', 'w', encoding='utf-8') as s:
+        for k, v in karakterer.items():
+            s.write(f'{k} {v}')
+            s.write('\n')
+
+
+def last():
+    global emner
+    global karakterer
+    global fagområde
+
+    emner = []
+    with open('hi2_emner.txt', 'r', encoding='utf-8') as a:
+        for i in a:
+            emner.append(i[:-1])
+
+    karakterer = {}
+    with open('hi2_karakterer.txt', 'r', encoding='utf-8') as s:
+        for i in s:
+            k, v = i.split(' ')
+            karakterer[k] = v[:-1]
+
+    fagområde = {}
+    with open('hi2_fagområde.txt', 'r', encoding='utf-8') as s:
+        for i in s:
+            k, v = i.split(' ')
+            fagområde[k] = v[:-1]
+
+
+def meny():
     print("""--------------------
 1 - Emneliste
 2 - Legg til Emne
 3 - Sett karakter
 4 - Karaktersnitt
 5 - Avslutt
+6 - Lagring
 --------------------""")
-    choice = input('Velg handling: ')
-    match choice:  # bruker match case istedefor if/elif/else (Krever Python 3.10) syns det ser ryddigere ut.
-        case '1':
-            emneliste()
-        case '2':
-            add()
-        case '3':
-            karakter()
-        case '4':
-            karaktersnitt()
-        case '5':
-            sys.exit()
-        case _:
-            print('Velg 1-5.')
 
 
-while True:
-    velg()
+def start():
+    meny()
+    last()
+    while True:
+        choice = input('Velg handling (0 for meny)> ')
+        match choice:  # bruker match case istedefor if/elif/else (Krever Python 3.10) syns det ser ryddigere ut.
+            case '0':
+                meny()
+            case '1':
+                print('Velg fag og/eller emnenivå (<enter> for alle)')
+                field = field_get()
+                nivå = nivå_get()
+
+                a = get_subjects(field, nivå)
+                b = get_graded_subjects(a)
+                show_graded(b)
+            case '2':
+                add()
+            case '3':
+                karakter_sett()
+            case '4':
+                print('Velg fag og/eller emnenivå (<enter> for alle)')
+                field = field_get()
+                nivå = nivå_get()
+
+                a = get_subjects(field, nivå)
+                b = get_graded_subjects(a)
+                print(karaktersnitt(b))
+            case '5':
+                save = input('Vil du lagre endringene? (j/n) ')
+                if save == 'j':
+                    lagre()
+                return
+            case '6':
+                lagre()
+            case _:
+                print('Velg 0-6.')
+
+
+start()
