d8c12a284a4ec0e7fbd00cd30230f1a444e9c7f8
Emmanator <andreassen1001@gmail.com>
2022-04-07 20:44:33 +0200

    ti7

---
+++ Data110/Obligatoriske Oppgaver/jij014_ti7.py
@@ -0,0 +1,30 @@
+import tkinter
+
+
+# Oppgave 1
+gui = tkinter.Tk()
+
+ramme = tkinter.Frame(gui)
+ramme.pack()
+knapp = tkinter.Button(ramme, text='Goodbye.', command=lambda: gui.destroy())
+knapp.pack()
+gui.mainloop()
+
+
+# Oppgave 2
+gui2 = tkinter.Tk()
+
+teller = tkinter.IntVar()
+teller.set(0)
+
+
+# Funksjon for å trykke på knappen
+def trykk(var, value):
+    var.set(var.get() + value)
+
+
+ramme2 = tkinter.Frame(gui2)
+ramme2.pack()
+knapp2 = tkinter.Button(ramme2, textvariable=teller, command=lambda: trykk(teller, 1))
+knapp2.pack()
+gui2.mainloop()
