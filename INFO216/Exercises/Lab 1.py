3fc23f26d27d77ff4c4ebc9f632da36ef66dbbdc
Emmanator <andreassen1001@gmail.com>
2023-01-22 17:38:10 +0100

    Lab 1 complete

--- INFO216/Exercises/Lab 1.py
+++ INFO216/Exercises/Lab 1.py
@@ -1,5 +1,8 @@
-from rdflib import Graph, Literal, RDF, URIRef, Namespace
-from rdflib.namespace import FOAF, XSD
+from rdflib import Graph, Literal, RDF, Namespace
+from rdflib.namespace import FOAF
+import requests
+import urllib
+from urllib import parse
 
 g = Graph()
 
@@ -9,6 +12,8 @@ robert = custom.Robert_Mueller
 donald = custom.Donald_Trump
 paul = custom.Paul_Manafort
 rick = custom.Rick_Gates
+cohen = custom.Michael_Cohen
+flynn = custom.Michael_Flynn
 investigation = custom.Mueller_Investigation
 
 g.add((robert, RDF.type, FOAF.Person))
@@ -22,8 +27,8 @@ g.add((donald, FOAF.name, Literal("Donald Trump")))
 people = [(paul, "Paul Manafort"),
           (rick, "Rick Gates"),
           (custom.George_Papadopoulos, "George Papadopoulos"),
-          (custom.Micheal_Flynn, "Michael Flynn"),
-          (custom.Micheal_Cohen, "Michael Cohen"),
+          (flynn, "Michael Flynn"),
+          (cohen, "Michael Cohen"),
           (custom.Roger_Stone, "Roger Stone")]
 
 # a for loop to simplify adding these people to the graph
@@ -50,7 +55,16 @@ g.add((rick, custom.charged, custom.tax_evasion))
 g.add((rick, custom.charged, custom.money_laundering))
 g.add((rick, custom.charged, custom.foreign_lobbying))
 g.add((rick, custom.plead_guilty, custom.conspiracy))
-g.add((rick, custom.lie, custom.fbi))
+g.add((rick, custom.plead_guilty, custom.lying_to_fbi))
+
+# Michael Cohen things
+g.add((cohen, custom.attorney, donald))
+g.add((cohen, custom.plead_guilty, custom.lying_to_congress))
+
+# Michael Flynn Things
+g.add((flynn, custom.adviser, donald))
+g.add((flynn, custom.plead_guilty, custom.lying_to_fbi))
+g.add((flynn, custom.negotiate, custom.plea_deal))
 
 # For ever triple in G, if G == plead_guilty, print triple
 for subj, pred, obj in g:
@@ -58,4 +72,16 @@ for subj, pred, obj in g:
         print(subj, pred, obj)
 
 # Personally I found XML to be the cleanest for this, but the other options work too.
-print(g.serialize(format='xml'))
+print(g.serialize(format='ttl'))
+
+
+# Method that gets information from rdf-grapher and saves it to file
+def get_image(graph):
+    g = urllib.parse.quote_plus(graph.serialize(format='ttl'))
+    response = requests.get(f'http://www.ldf.fi/service/rdf-grapher?rdf={g}&from=ttl&to=png')
+    response.raise_for_status()
+    with open("response.png", "wb") as f:
+        f.write(response.content)
+
+
+get_image(g)
