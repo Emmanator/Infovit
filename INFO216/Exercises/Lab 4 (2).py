e0b94198197018f01bbd8d338e7abea1db04c2a8
Emmanator <andreassen1001@gmail.com>
2023-02-25 17:04:13 +0100

    Lab 4 info216

--- INFO216/Exercises/Lab 4 (2).py
+++ INFO216/Exercises/Lab 4 (2).py
@@ -1,7 +1,10 @@
-from SPARQLWrapper import SPARQLWrapper
+from SPARQLWrapper import SPARQLWrapper, JSON, XML
+from rdflib import Graph
 
 SERVER = 'http://localhost:9999/blazegraph/'
 NAMESPACE = 'ns1'
+
+g = Graph()
 
 endpoint = f'{SERVER}namespace/{NAMESPACE}/sparql'
 client = SPARQLWrapper(endpoint)
@@ -37,7 +40,7 @@ prefix ns1: <http://example.org#>
 
 SELECT *
 WHERE{
-    ?s ?p ?o
+    ?s ns1:person ?o
 }
 """
 # s = set()
@@ -48,17 +51,85 @@ WHERE{
 # print(s)
 
 
-client.setQuery(types)
+gwaph_update = """
+prefix ns1: <http://example.org#>
+
+INSERT {?o rdf:type ns1:IndictedPerson}
+
+WHERE {
+    ?s ns1:person ?o
+} """
+
+
+title_update = """
+prefix ns1: <http://example.org#>
+
+INSERT {?o dc:title ?test}
+
+WHERE {
+    ?s ns1:investigation ?o
+    BIND(REPLACE(REPLACE(REPLACE(STR(?o), STR(ns1:), ''), '_', ' '), '%2C', ',') AS ?test)
+}
+
+"""
+
+indicted_sorted = """
+prefix ns1: <http://example.org#>
+
+SELECT *
+
+WHERE {
+    ?s rdf:type ns1:IndictedPerson
+}
+ORDER BY ?s
+"""
+
+
+indicted_days = """
+prefix ns1: <http://example.org#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+SELECT (MAX(?t) AS ?max) (MIN(?t) AS ?min) (AVG(xsd:double(?t)) AS ?avg) ?i
+
+WHERE {
+    ?s ns1:indictment_days ?o ;
+        ns1:investigation ?i
+    BIND(REPLACE(STR(?o), STR(ns1:), '')AS ?t)
+    FILTER(?t != 'nan')
+}
+GROUP BY ?i
+"""
+
+
+describe_stone = """
+prefix ns1: <http://example.org#>
+
+DESCRIBE ns1:Oliver_North
+"""
+
+client.setQuery(describe_stone)
+# client.method = 'POST'
+# client.query()
+client.setReturnFormat(XML)
+
+
 # print('Spouses:')
 results = client.queryAndConvert()
-s = set()
-for row in results["results"]["bindings"]:
-    for r in row:
-        s.add(row[r]["type"])
+s = list()
+
+#for row in results["results"]["bindings"]:
+#    for r in row:
+#        print(row[r]['value'])
+        # if row[r]['value'] != 'nan':
+        #     s.append(float(row[r]['value']))
+
     # print(row)
-print(s)
+# print(sum(s)/len(s))
 
 # FILTER  (?start < "1990-01-01")
 # FILTER  (?end > "2090-01-01")
 # ["name"]["value"]
 # BIND(REPLACE(REPLACE(REPLACE(STR(?o), STR(ns1:), ''), '_', ' '), '%2C', ',') AS ?test)
+
+print(type(results))
+print(results.serialize(format='ttl'))
